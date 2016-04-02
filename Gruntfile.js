52bd6a225300373a36c1143920b99dcf7df3f90e
elliot <postsneakernet@gmail.com>
2016-04-02 19:13:24 +0000

    Update project requirements

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,12 +7,32 @@ module.exports = function (grunt) {
             files: [
                 'Gruntfile.js',
                 '<%= pkg.name %>/assets/js/spa/*.js',
-                '<%= pkg.name %>/assets/js/custom.js'
+                '<%= pkg.name %>/assets/js/custom.js',
+                '<%= pkg.name %>/assets/js/admin-handlebars.js'
             ],
             options: {
                 globals: {
                     jQuery: true
                 }
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    '<%= pkg.name %>/assets/css/custom.css': '<%= pkg.name %>/assets/css/custom.scss'
+                }
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                        '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: [
+                    '<%= pkg.name %>/assets/js/custom.js'
+                ],
+                dest: '<%= pkg.name %>/assets/js/custom.min.js'
             }
         },
         copy: {
@@ -50,8 +70,11 @@ module.exports = function (grunt) {
                     'bower_components/angular-route/angular-route.min.js',
                     'bower_components/jquery/dist/jquery.js',
                     'bower_components/jquery/dist/jquery.min.js',
+                    'bower_components/jquery-validation/dist/jquery.validate.min.js',
                     'bower_components/bootstrap/dist/js/bootstrap.js',
-                    'bower_components/bootstrap/dist/js/bootstrap.min.js'
+                    'bower_components/bootstrap/dist/js/bootstrap.min.js',
+                    'bower_components/handlebars/handlebars.js',
+                    'bower_components/handlebars/handlebars.min.js'
                 ],
                 dest: '<%= pkg.name %>/static/js'
             },
@@ -72,18 +95,6 @@ module.exports = function (grunt) {
                 dest: '<%= pkg.name %>/static/fonts'
             }
         },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-                        '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: [
-                    '<%= pkg.name %>/assets/js/custom.js'
-                ],
-                dest: '<%= pkg.name %>/assets/js/custom.min.js'
-            }
-        },
         watch: {
             files: ['<%= jshint.files %>'],
             tasks: ['jshint']
@@ -91,10 +102,10 @@ module.exports = function (grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['jshint']);
-    grunt.registerTask('def', ['jshint', 'uglify', 'copy']);
+    grunt.registerTask('default', ['jshint', 'sass', 'uglify', 'copy']);
 };
