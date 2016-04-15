8b3aad6d665004686aeb5f538312e56405d50db7
elliot <postsneakernet@gmail.com>
2016-04-15 00:58:11 +0000

    Clean up js

--- issuetracker/assets/js/admin-handlebars.js
+++ issuetracker/assets/js/admin-handlebars.js
@@ -1,6 +1,5 @@
 var admin = (function () {
     $(document).ready(function () {
-        console.log("admin iife was called");
         admin.getAdminTemplate(5000);
     });
     $("#admin-home").click(function () {
@@ -25,7 +24,6 @@ var admin = (function () {
     });
 
     var getMsgTemplate = function (templateId, msg) {
-        console.log("admin.getMsgTemplate was called");
         var source = $(templateId).html();
         var template = Handlebars.compile(source);
         var context = {"js_msg": msg};
@@ -34,13 +32,11 @@ var admin = (function () {
     };
 
     var checkResult = function (context, cb) {
-        console.log("admin.parseContextResults was called");
         if (context.hasOwnProperty('error')) {
             getMsgTemplate("#error-template", context.error);
         } else if (context.hasOwnProperty('success')) {
             getMsgTemplate("#success-template", context.success);
             if (undefined !== cb) {
-                console.log("calling callback function passed to sendFormData > checkResult");
                 cb();
             }
             // move into own function
@@ -52,7 +48,6 @@ var admin = (function () {
         if (t === undefined) {
             t = 0;
         }
-        console.log("timer value: " + t);
         setTimeout(function () {
             $(".flask-msg-placeholder").empty();
             $(".js-msg-placeholder").empty();
@@ -60,7 +55,6 @@ var admin = (function () {
     };
 
     var getTemplate = function (t, pageTitle, templateId, context, url, func, cb) {
-        console.log("admin.getTemplate was called with: " + arguments);
         clearMsg(t);
         $("#page-title").text(pageTitle);
         var source = $(templateId).html();
@@ -75,38 +69,32 @@ var admin = (function () {
     };
 
     var remoteJSON = function (url, template, cb) {
-        console.log("admin.remoteJSON was called");
         $.getJSON(url, {
         })
         .done(function (data) {
-            console.log("ajax GET to " + url);
             var context = data;
             checkResult(context);
             var html = template(context);
             $(".content-placeholder").html(html);
             if (undefined !== cb) {
-                console.log("calling callback function passed to remoteJSON");
                 cb();
             }
         })
         .fail(function (e) {
-            console.log("fail from ajax GET to " + url);
             getMsgTemplate("#error-template", "There was an issue communicating with the server.");
         });
     };
 
     var getAdminTemplate = function (t) {
-        console.log("admin.getAdminTemplate was called");
         getTemplate(t, "Admin", "#admin-template", undefined, "/ajax/admin/", remoteJSON);
     };
 
     var getMaintainersTemplate = function (t) {
-        console.log("admin.getMaintainersTemplate was called");
-        getTemplate(t, "Maintainers", "#maintainers-template", undefined, "/ajax/admin/maintainers/", remoteJSON);
+        getTemplate(t, "Maintainers", "#maintainers-template", undefined,
+                "/ajax/admin/maintainers/", remoteJSON);
     };
 
     var getMaintainersCreateTemplate = function (t) {
-        console.log("admin.getMaintainersCreateTemplate was called");
         getTemplate(t, "Maintainers", "#maintainers-create-template");
         $("#maintainers-create-form").validate({
             rules: {
@@ -122,14 +110,15 @@ var admin = (function () {
                 }
             },
             submitHandler: function () {
-                admin.sendFormData("/ajax/admin/maintainers/create/", admin.getFormData("#maintainers-create-form"));
+                admin.sendFormData("/ajax/admin/maintainers/create/",
+                        admin.getFormData("#maintainers-create-form"));
             }
         });
     };
 
     var getMaintainersUpdateTemplate = function (t, path) {
-        console.log("admin.getMaintainersUpdateTemplate was called");
-        getTemplate(t, "Maintainers", "#maintainers-update-template", undefined, "/ajax/admin/maintainers/update/" + path, remoteJSON, function () {
+        getTemplate(t, "Maintainers", "#maintainers-update-template", undefined,
+                "/ajax/admin/maintainers/update/" + path, remoteJSON, function () {
                 $("#maintainers-update-form").validate({
                     rules: {
                         username: "required",
@@ -144,18 +133,18 @@ var admin = (function () {
                         }
                     },
                     submitHandler: function () {
-                        admin.sendFormData("/ajax/admin/maintainers/update/" + path, admin.getFormData("#maintainers-update-form"), function () {
+                        admin.sendFormData("/ajax/admin/maintainers/update/" + path,
+                                admin.getFormData("#maintainers-update-form"), function () {
                             getMaintainersTemplate(5000);
                         });
                     }
                 });
         });
-        console.log("Does it exist yet: " + $("#maintainers-update-form").length);
     };
 
     var getProjectsTemplate = function (t) {
-        console.log("admin.getProjectTemplate was called");
-        getTemplate(t, "Projects", "#projects-template", undefined, "/ajax/admin/projects/", remoteJSON, function () {
+        getTemplate(t, "Projects", "#projects-template", undefined, "/ajax/admin/projects/",
+                remoteJSON, function () {
             $(function () {
                 $('[data-toggle="tooltip"]').tooltip();
             });
@@ -163,41 +152,42 @@ var admin = (function () {
     };
 
     var getProjectsCreateTemplate = function (t) {
-        console.log("admin.getProjectsCreateTemplate was called");
-        getTemplate(t, "Projects", "#projects-create-template", undefined, "/ajax/admin/projects/create/", remoteJSON, function () {
+        getTemplate(t, "Projects", "#projects-create-template", undefined,
+                "/ajax/admin/projects/create/", remoteJSON, function () {
                 $("#projects-create-form").validate({
                     rules: {
                         title: "required",
                         description: "required",
                     },
                     submitHandler: function () {
-                        admin.sendFormData("/ajax/admin/projects/create/", admin.getFormData("#projects-create-form"));
+                        admin.sendFormData("/ajax/admin/projects/create/",
+                                admin.getFormData("#projects-create-form"));
                     }
                 });
         });
     };
 
     var getProjectsUpdateTemplate = function (t, path) {
-        console.log("admin.getProjectsUpdateTemplate was called");
-        getTemplate(t, "Projects", "#projects-update-template", undefined, "/ajax/admin/projects/update/" + path, remoteJSON, function () {
+        getTemplate(t, "Projects", "#projects-update-template", undefined,
+                "/ajax/admin/projects/update/" + path, remoteJSON, function () {
                 $("#projects-update-form").validate({
                     rules: {
                         title: "required",
                         description: "required",
                     },
                     submitHandler: function () {
-                        admin.sendFormData("/ajax/admin/projects/update/" + path, admin.getFormData("#projects-update-form"), function () {
+                        admin.sendFormData("/ajax/admin/projects/update/" + path,
+                                admin.getFormData("#projects-update-form"), function () {
                             getProjectsTemplate(5000);
                         });
                     }
                 });
         });
-        console.log("Does it exist yet: " + $("#projects-update-form").length);
     };
 
     var getTicketsTemplate = function (t) {
-        console.log("admin.getTicketsTemplate was called");
-        getTemplate(t, "Tickets", "#tickets-template", undefined, "/ajax/admin/tickets/", remoteJSON, function () {
+        getTemplate(t, "Tickets", "#tickets-template", undefined, "/ajax/admin/tickets/",
+                remoteJSON, function () {
             $(function () {
                 $('[data-toggle="tooltip"]').tooltip();
             });
@@ -205,8 +195,8 @@ var admin = (function () {
     };
 
     var getTicketsCreateTemplate = function (t) {
-        console.log("admin.getTicketsCreateTemplate was called");
-        getTemplate(t, "Tickets", "#tickets-create-template", undefined, "/ajax/admin/tickets/create/", remoteJSON, function () {
+        getTemplate(t, "Tickets", "#tickets-create-template", undefined,
+                "/ajax/admin/tickets/create/", remoteJSON, function () {
                 $("#tickets-create-form").validate({
                     rules: {
                         name: "required",
@@ -218,15 +208,16 @@ var admin = (function () {
                         content: "required",
                     },
                     submitHandler: function () {
-                        admin.sendFormData("/ajax/admin/tickets/create/", admin.getFormData("#tickets-create-form"));
+                        admin.sendFormData("/ajax/admin/tickets/create/",
+                                admin.getFormData("#tickets-create-form"));
                     }
                 });
         });
     };
 
     var getTicketsUpdateTemplate = function (t, path) {
-        console.log("admin.getTicketsUpdateTemplate was called");
-        getTemplate(t, "Tickets", "#tickets-update-template", undefined, "/ajax/admin/tickets/update/" + path, remoteJSON, function () {
+        getTemplate(t, "Tickets", "#tickets-update-template", undefined,
+                "/ajax/admin/tickets/update/" + path, remoteJSON, function () {
                 $("#tickets-update-form").validate({
                     rules: {
                         name: "required",
@@ -238,18 +229,18 @@ var admin = (function () {
                         content: "required",
                     },
                     submitHandler: function () {
-                        admin.sendFormData("/ajax/admin/tickets/update/" + path, admin.getFormData("#tickets-update-form"), function () {
+                        admin.sendFormData("/ajax/admin/tickets/update/" + path,
+                                admin.getFormData("#tickets-update-form"), function () {
                             getTicketsTemplate(5000);
                         });
                     }
                 });
         });
-        console.log("Does it exist yet: " + $("#tickets-update-form").length);
     };
 
     var getCommentsTemplate = function (t) {
-        console.log("admin.getCommentsTemplate was called");
-        getTemplate(t, "Comments", "#comments-template", undefined, "/ajax/admin/comments/", remoteJSON, function () {
+        getTemplate(t, "Comments", "#comments-template", undefined, "/ajax/admin/comments/",
+                remoteJSON, function () {
             $(function () {
                 $('[data-toggle="tooltip"]').tooltip();
             });
@@ -257,8 +248,8 @@ var admin = (function () {
     };
 
     var getCommentsCreateTemplate = function (t) {
-        console.log("admin.getCommentsCreateTemplate was called");
-        getTemplate(t, "Comments", "#comments-create-template", undefined, "/ajax/admin/comments/create/", remoteJSON, function () {
+        getTemplate(t, "Comments", "#comments-create-template", undefined,
+                "/ajax/admin/comments/create/", remoteJSON, function () {
                 $("#comments-create-form").validate({
                     rules: {
                         name: "required",
@@ -269,15 +260,16 @@ var admin = (function () {
                         content: "required",
                     },
                     submitHandler: function () {
-                        admin.sendFormData("/ajax/admin/comments/create/", admin.getFormData("#comments-create-form"));
+                        admin.sendFormData("/ajax/admin/comments/create/",
+                                admin.getFormData("#comments-create-form"));
                     }
                 });
         });
     };
 
     var getCommentsUpdateTemplate = function (t, path) {
-        console.log("admin.getCommentsUpdateTemplate was called");
-        getTemplate(t, "Comments", "#comments-update-template", undefined, "/ajax/admin/comments/update/" + path, remoteJSON, function () {
+        getTemplate(t, "Comments", "#comments-update-template", undefined,
+                "/ajax/admin/comments/update/" + path, remoteJSON, function () {
                 $("#comments-update-form").validate({
                     rules: {
                         name: "required",
@@ -288,26 +280,23 @@ var admin = (function () {
                         content: "required",
                     },
                     submitHandler: function () {
-                        admin.sendFormData("/ajax/admin/comments/update/" + path, admin.getFormData("#comments-update-form"), function () {
+                        admin.sendFormData("/ajax/admin/comments/update/" + path,
+                                admin.getFormData("#comments-update-form"), function () {
                             getCommentsTemplate(5000);
                         });
                     }
                 });
         });
-        console.log("Does it exist yet: " + $("#comments-update-form").length);
     };
 
     var getFormData = function (formId) {
-        console.log("admin.getFormData was called");
         var formInputs = $(formId + ' :input').not(':input[type=button], :input[type=submit]');
         var obj = {};
         formInputs.each(function () {
             var cc = $.camelCase(this.id);
             if ($(this).is(':checkbox')) {
-                console.log("I am a checkbox");
                 obj[cc] = $(this).is(':checked');
             } else {
-                console.log("I am not a checkbox");
                 obj[cc] = $(this).val();
             }
         });
@@ -315,7 +304,6 @@ var admin = (function () {
     };
 
     var sendFormData = function (url, data, cb) {
-        console.log("admin.sendFormData was called");
         $.ajax({
             url: url,
             type: "POST",
@@ -324,11 +312,9 @@ var admin = (function () {
             dataType: "json"
         })
         .done(function (data) {
-            console.log("ajax POST to " + url);
             checkResult(data, cb);
         })
         .fail(function (e) {
-            console.log("fail from ajax POST to " + url);
             getMsgTemplate("#error-template", "There was an issue communicating with the server.");
         });
     };
